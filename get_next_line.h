7875cba9bf94aee21d25e14b2e97ef0fa8edc136
lmelo-do <lmelo-do@student.42sp.org.br>
2025-07-30 19:34:31 -0300

    fix: update header timestamp and remove unused function declarations

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lmelo-do <lmelo-do@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 21:14:24 by lmelo-do          #+#    #+#             */
-/*   Updated: 2025/07/30 16:01:17 by lmelo-do         ###   ########.fr       */
+/*   Updated: 2025/07/30 19:33:24 by lmelo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,10 @@
 # include <unistd.h>
 
 char	*get_next_line(int fd);
-char	*ft_read(int fd, char *stash);
-char	*ft_extract_line(char *stash);
-char	*ft_trim_stash(char *stash);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s);
 size_t	ft_strlen(const char *s);
 void	*ft_calloc(size_t n, size_t size);
 void	*ft_free(void *ptr, void *ptr2);
-char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
 
 #endif
