469c066fb8602cac1b4941cd21468417a8ad5435
lmelo-do <lmelo-do@student.42sp.org.br>
2025-07-29 21:39:28 -0300

    fix: update header file with correct update timestamp and adjust include guards

--- get_next_line.h
+++ get_next_line.h
@@ -6,27 +6,30 @@
 /*   By: lmelo-do <lmelo-do@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 21:14:24 by lmelo-do          #+#    #+#             */
-/*   Updated: 2025/07/29 15:25:29 by lmelo-do         ###   ########.fr       */
+/*   Updated: 2025/07/29 20:42:08 by lmelo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE
-# define GET_NEXT_LINE
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
 char	*ft_read_and_store(int fd, char *resto);
 char	*ft_extract_line(char *resto);
 char	*ft_update_line(char *resto);
 char	*ft_strjoin(char const *s1, char const *s2);
-void	*ft_calloc(size_t n, size_t size);
+char	*ft_strdup(const char *s);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
+void	*ft_calloc(size_t n, size_t size);
+void	*ft_free(void *ptr, void *ptr2);
+char	*ft_strchr(const char *s, int c);
 
 #endif
